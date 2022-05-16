ba28303acefeb4db6b0b2c285d840151f5c74399
Daniel Duan <daniel@duan.ca>
2022-05-16 16:05:43 -0700

    Implement portable stat for Windows

--- Sources/SystemExtras/Internals.swift
+++ Sources/SystemExtras/Internals.swift
@@ -1,14 +1,14 @@
 #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
 import Darwin
 #elseif os(Linux) || os(FreeBSD) || os(Android)
-import CSystem
 import Glibc
 #elseif os(Windows)
-import CSystem
 import ucrt
 #else
 #error("Unsupported Platform")
 #endif
+
+import SystemPackage
 
 #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
 internal var system_errno: CInt {
@@ -31,5 +31,17 @@ internal var system_errno: CInt {
 internal var system_errno: CInt {
   get { Glibc.errno }
   set { Glibc.errno = newValue }
+}
+#endif
+
+#if os(Windows)
+typealias system_stat_struct = _stat64i32
+func system_stat(_ path: UnsafePointer<CInterop.PlatformChar>, _ result: inout _stat64i32) -> CInt {
+  _wstat64i32(path, &result)
+} 
+#else
+typealias system_stat_struct = stat
+func system_stat(_ path: UnsafePointer<CInterop.PlatformChar>, _ result: inout stat) -> CInt {
+  stat(path, &result)
 }
 #endif
