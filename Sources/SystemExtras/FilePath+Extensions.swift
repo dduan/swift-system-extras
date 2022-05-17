b54ee67bd6a57d2d6d426a9a36c57bddd81ad06a
Daniel Duan <daniel@duan.ca>
2022-05-17 02:25:13 -0700

    Implement directory content

--- Sources/SystemExtras/FilePath+Extensions.swift
+++ Sources/SystemExtras/FilePath+Extensions.swift
@@ -1,15 +1,6 @@
 import SystemPackage
 
 extension FilePath {
-    /// Return `true` if path refers to an existing path.
-    /// On some platforms, this function may return `false` if permission is not
-    /// granted to retrieve metadata on the requested file, even if the path physically exists.
-    ///
-    /// - Returns: whether path refers to an existing path or an open file descriptor.
-    public func exists() -> Bool {
-        (try? self.metadata()) != nil
-    }
-
     /// Create a directory, and, optionally, any intermediate directories that leads to it, if they don't
     /// exist yet.
     ///
--- Sources/SystemExtras/Metadata.swift
+++ Sources/SystemExtras/Metadata.swift
@@ -16,4 +16,13 @@ extension FilePath {
     let permissions = FilePermissions(rawValue: CInterop.Mode(status.st_mode) & 0o7777)
     return FileMetadata(permissions: permissions)
   }
+
+  /// Return `true` if path refers to an existing path.
+  /// On some platforms, this function may return `false` if permission is not
+  /// granted to retrieve metadata on the requested file, even if the path physically exists.
+  ///
+  /// - Returns: whether path refers to an existing path or an open file descriptor.
+  public func exists() -> Bool {
+    (try? self.metadata()) != nil
+  }
 }
