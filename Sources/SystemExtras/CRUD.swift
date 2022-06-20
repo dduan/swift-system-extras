ec1a9b937ab8c070946dc2124302f110d792551c
Daniel Duan <daniel@duan.ca>
2022-06-20 14:01:43 -0700

    fixup! Implement FilePath.move(to:)

--- Sources/SystemExtras/CRUD.swift
+++ Sources/SystemExtras/CRUD.swift
@@ -71,9 +71,17 @@ extension FilePath {
     public func move(to newPath: FilePath) throws {
         try self.withPlatformString { sourceCString in
             try newPath.withPlatformString { targetCString in
-                if system_rename(sourceCString, targetCString) != 0 {
+#if os(Windows)
+                if !MoveFileW(sourceCString, targetCString) {
+                    let _ = GetLastError()
+                    // TODO: map WinAPI error to errno when Swift Systems does
+                    throw Errno(rawValue: -1)
+                }
+#else
+                if rename(sourceCString, targetCString) != 0 {
                     throw Errno(rawValue: system_errno)
                 }
+#endif
             }
         }
     }
