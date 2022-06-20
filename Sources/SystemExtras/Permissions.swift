d4edba9835daca9e4ad81340372fa82237e5fdfd
Daniel Duan <daniel@duan.ca>
2022-06-20 13:06:15 -0700

    fixup! Implement FilePath.set(_:)

--- Sources/SystemExtras/Permissions.swift
+++ Sources/SystemExtras/Permissions.swift
@@ -1,7 +1,8 @@
 import SystemPackage
 
 extension FilePath {
-    public func set(_ permissions: FilePermissions) throws {
+    @available(macOS 12, *)
+    public func set(posix permissions: FilePermissions) throws {
         try self.withPlatformString { cString in
             if system_chmod(cString, permissions.rawValue) != 0 {
                 throw Errno(rawValue: system_errno)
--- Tests/SystemExtrasTests/Permissions.swift
+++ Tests/SystemExtrasTests/Permissions.swift
@@ -3,13 +3,14 @@ import SystemPackage
 import XCTest
 
 final class PermissionsTests: XCTestCase {
+    @available(macOS 12, *)
     func testSettingPermissions() throws {
         try FilePath.withTemporaryDirectory { temp in
             let file = temp.pushing("a")
             try file.write(utf8: "hello")
             let oldPermissions = try file.metadata().permissions
             XCTAssert(oldPermissions.contains(.ownerWrite))
-            try file.set(oldPermissions.subtracting([.ownerWrite]))
+            try file.set(posix: oldPermissions.subtracting([.ownerWrite]))
             let newPermissions = try file.metadata().permissions
             XCTAssertFalse(newPermissions.contains(.ownerWrite))
         }
