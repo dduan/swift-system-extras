72aa0c8aa1494e3eb826a3426b71abe02c8ed919
Daniel Duan <daniel@duan.ca>
2022-06-25 12:03:54 -0700

    Support followSymlink in exists (#35)
    Closes #29

--- Sources/SystemExtras/FileMetadata.swift
+++ Sources/SystemExtras/FileMetadata.swift
@@ -59,13 +59,16 @@ extension FilePath {
     }
 #endif // os(Windows)
 
-  /// Return `true` if path refers to an existing path.
-  /// On some platforms, this function may return `false` if permission is not
-  /// granted to retrieve metadata on the requested file, even if the path physically exists.
-  ///
-  /// - Returns: whether path refers to an existing path or an open file descriptor.
-    public func exists() -> Bool {
-        (try? self.metadata()) != nil
+    /// Return `true` if path refers to an existing path.
+    /// On some platforms, this function may return `false` if permission is not
+    /// granted to retrieve metadata on the requested file, even if the path physically exists.
+    ///
+    /// - Parameter followSymlink: whether to follow symbolic links. If `true`, return `false` for
+    ///                            broken symbolic links.
+    ///
+    /// - Returns: whether path refers to an existing path or an open file descriptor.
+    public func exists(followSymlink: Bool = false) -> Bool {
+        (try? self.metadata(followSymlink: followSymlink)) != nil
     }
 
 #if os(Windows)
